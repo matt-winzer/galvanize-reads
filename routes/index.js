beb4bd7865e19ee8216bfd974b815737c4c70f64
matt-winzer <matt.winzer@gmail.com>
2017-01-12 10:56:16 -0700

    :sparkles: completed route for posting new book

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -16,8 +16,19 @@ router.get('/books', function(req, res, next) {
     });
 });
 
+// render new book form
 router.get('/books/new', function(req, res, next) {
   res.render('newbook');
+});
+
+// POST new book
+router.post('/', function (req, res) {
+  knex('book')
+    .insert(req.body)
+    .returning('id')
+    .then((id) => {
+      res.redirect('/books');
+    });
 });
 
 module.exports = router;
